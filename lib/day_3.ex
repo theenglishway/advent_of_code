fe3593600256951a30a9da6f0d435d92b642bafd
theenglishway (time) <me@theenglishway.eu>
2021-12-03 22:39:41 +0100

    Cleanup

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -14,10 +14,12 @@ defmodule AdventOfCode.Day3 do
       nb_reports = Enum.count(report_list)
       initial_value = Stream.cycle([0]) |> Enum.take(entry_width)
 
-      ones_count =
+      numbers =
         report_list
-        |> Day3.to_number()
-        |> Enum.map(&Day3.to_binary_repr(&1, entry_width))
+        |> Day3.to_binary_representation()
+
+      ones_count =
+        numbers
         |> Enum.reduce(initial_value, &Day3.pairwise_sum/2)
 
       gamma =
@@ -37,13 +39,10 @@ defmodule AdventOfCode.Day3 do
       ...> ])
       {23, 10}
     """
-    def get_oxygen_and_co2(report_list = [first | _]) when is_list(report_list) do
-      entry_width = first |> String.length()
-
+    def get_oxygen_and_co2(report_list) when is_list(report_list) do
       numbers =
         report_list
-        |> Day3.to_number()
-        |> Enum.map(&Day3.to_binary_repr(&1, entry_width))
+        |> Day3.to_binary_representation()
 
       oxygen =
         numbers
@@ -65,26 +64,42 @@ defmodule AdventOfCode.Day3 do
 
     defp apply_criteria(numbers_list, value_fun, index) when is_function(value_fun, 2) do
       count = Enum.count(numbers_list)
-      number_of_ones = numbers_list |> Enum.map(&(&1 |> Enum.at(index))) |> Enum.sum()
-      value = value_fun.(number_of_ones, count)
+      value = numbers_list |> count_ones_at(index) |> value_fun.(count)
 
       numbers_list
       |> Enum.filter(&(&1 |> Enum.at(index) == value))
       |> apply_criteria(value_fun, index + 1)
     end
+
+    defp count_ones_at(numbers_list, index),
+      do: numbers_list |> Enum.map(&(&1 |> Enum.at(index))) |> Enum.sum()
   end
 
-  def to_number(string_list) when is_list(string_list), do: string_list |> Enum.map(&to_number/1)
-  def to_number(string) when is_binary(string), do: string |> String.to_integer(2)
+  @doc """
+  Turn a list of binary number string representations into a `homemade` binary representation
 
-  def to_binary_repr(number, width) when is_integer(number),
-    do: number |> Integer.digits(2) |> pad_leading(width)
+    iex> AdventOfCode.Day3.to_binary_representation(["10100"])
+      [[1, 0, 1, 0, 0]]
+    iex> AdventOfCode.Day3.to_binary_representation(["00100"])
+      [[0, 0, 1, 0, 0]]
+  """
+  @spec to_binary_representation([binary]) :: [[integer]]
+  def to_binary_representation(string_list = [head | _]) when is_list(string_list) do
+    entry_width = head |> String.length()
 
-  def pad_leading(list, target) when is_list(list),
+    string_list
+    |> Enum.map(&String.to_integer(&1, 2))
+    |> Enum.map(&(&1 |> Integer.digits(2) |> pad_leading(entry_width)))
+  end
+
+  # Add padding zeros to the start of `list`, until it reaches a size of `target`
+  defp pad_leading(list, target) when is_list(list),
     do: list |> pad_leading(target, Enum.count(list))
 
-  def pad_leading(list, target, size) when size < target, do: ([0] ++ list) |> pad_leading(target)
-  def pad_leading(list, target, size) when size == target, do: list
+  defp pad_leading(list, target, size) when size < target,
+    do: ([0] ++ list) |> pad_leading(target)
+
+  defp pad_leading(list, target, size) when size == target, do: list
 
   def pairwise_sum(nb1, nb2), do: nb1 |> Enum.zip_with(nb2, &(&1 + &2))
 
