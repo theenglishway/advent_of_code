82f9798a1eedf9c5360510c5523460c6aa88d047
theenglishway (time) <me@theenglishway.eu>
2021-12-03 22:14:12 +0100

    Solve 2nd half of day 3

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -30,6 +30,50 @@ defmodule AdventOfCode.Day3 do
     end
   end
 
+  defmodule SecondHalf do
+    @doc """
+      iex> AdventOfCode.Day3.SecondHalf.get_oxygen_and_co2([
+      ...>  "00100","11110","10110","10111","10101","01111","00111","11100","10000","11001","00010","01010"
+      ...> ])
+      {23, 10}
+    """
+    def get_oxygen_and_co2(report_list = [first | _]) when is_list(report_list) do
+      entry_width = first |> String.length()
+
+      numbers =
+        report_list
+        |> Day3.to_number()
+        |> Enum.map(&Day3.to_binary_repr(&1, entry_width))
+
+      oxygen =
+        numbers
+        |> apply_criteria(fn number_of_ones, count ->
+          if number_of_ones >= count / 2, do: 1, else: 0
+        end)
+
+      co2 =
+        numbers
+        |> apply_criteria(fn number_of_ones, count ->
+          if number_of_ones >= count / 2, do: 0, else: 1
+        end)
+
+      {oxygen, co2}
+    end
+
+    defp apply_criteria(numbers_list, value_fun, index \\ 0)
+    defp apply_criteria([singleton], _, _), do: singleton |> Integer.undigits(2)
+
+    defp apply_criteria(numbers_list, value_fun, index) when is_function(value_fun, 2) do
+      count = Enum.count(numbers_list)
+      number_of_ones = numbers_list |> Enum.map(&(&1 |> Enum.at(index))) |> Enum.sum()
+      value = value_fun.(number_of_ones, count)
+
+      numbers_list
+      |> Enum.filter(&(&1 |> Enum.at(index) == value))
+      |> apply_criteria(value_fun, index + 1)
+    end
+  end
+
   def to_number(string_list) when is_list(string_list), do: string_list |> Enum.map(&to_number/1)
   def to_number(string) when is_binary(string), do: string |> String.to_integer(2)
 
@@ -48,6 +92,12 @@ defmodule AdventOfCode.Day3 do
     do:
       get_input()
       |> FirstHalf.get_gamma_and_epsilon()
+      |> to_output()
+
+  def run(2),
+    do:
+      get_input()
+      |> SecondHalf.get_oxygen_and_co2()
       |> to_output()
 
   defp get_input() do
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -1,4 +1,5 @@
 defmodule AdventOfCodeDay3Test do
   use ExUnit.Case
   doctest AdventOfCode.Day3.FirstHalf
+  doctest AdventOfCode.Day3.SecondHalf
 end
