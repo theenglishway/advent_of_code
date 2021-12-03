464009f05e6adbd3aff7382bff3147c23224e1a1
theenglishway (time) <me@theenglishway.eu>
2021-12-03 23:20:12 +0100

    Run all exercises when no arg is passed in CLI

--- lib/mix/runner.ex
+++ lib/mix/runner.ex
@@ -1,13 +1,23 @@
 defmodule Mix.Tasks.Runner do
   @moduledoc "A simple runner for the exercises"
+
   use Mix.Task
   alias AdventOfCode.Exercise
+  @all_days 1..3
 
-  @shortdoc "Run exercise from day `day`"
   @impl Mix.Task
   def run([day, half]) do
-    Exercise.run(day |> String.to_integer(), half |> String.to_integer())
-    |> inspect
-    |> Mix.shell().info()
+    print_exercise({day |> String.to_integer(), half |> String.to_integer()})
+  end
+
+  def run([]) do
+    for(day <- @all_days, half <- 1..2, do: {day, half})
+    |> Enum.each(&print_exercise/1)
+  end
+
+  defp print_exercise({day, half}) do
+    result = Exercise.run(day, half)
+
+    Mix.shell().info("Day #{day} / Half #{half} : #{result}")
   end
 end
