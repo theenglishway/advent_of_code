c096d05e399015b2f0512810a7189efeaee2e7f6
theenglishway (time) <me@theenglishway.eu>
2021-12-05 11:41:14 +0100

    Convert input and add test

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -14,7 +14,19 @@ defmodule AdventOfCode.Day5 do
   @impl true
   def get_input() do
     with {:ok, data} <- File.read(@input),
-         do: data
+         do: data |> into_vents_lines()
+  end
+
+  def into_vents_lines(string) when is_binary(string) do
+    string
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      with [start_, end_] <- line |> String.trim() |> String.split(" -> "),
+           list_of_coords <-
+             [start_ |> String.split(","), end_ |> String.split(",")] |> Enum.concat(),
+           [start_x, start_y, end_x, end_y] <- list_of_coords |> Enum.map(&String.to_integer/1),
+           do: {:vent_line, {:coord, start_x, start_y}, {:coord, end_x, end_y}}
+    end)
   end
 
   @impl true
--- test/day05_test.exs
+++ test/day05_test.exs
@@ -1,4 +1,23 @@
 defmodule AdventOfCodeDay5Test do
   use ExUnit.Case
-  doctest AdventOfCode.Day5.FirstHalf
+  alias AdventOfCode.Day5
+  alias AdventOfCode.Day5.FirstHalf
+  doctest FirstHalf
+
+  @input """
+    0,9 -> 5,9
+    8,0 -> 0,8
+    9,4 -> 3,4
+    2,2 -> 2,1
+    7,0 -> 7,4
+    6,4 -> 2,0
+    0,9 -> 2,9
+    3,4 -> 1,4
+    0,0 -> 8,8
+    5,5 -> 8,2
+  """
+
+  test "should work on first half" do
+    assert @input |> Day5.into_vents_lines() |> FirstHalf.get_number_of_overlaps() == 5
+  end
 end
