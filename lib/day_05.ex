8b702024e9fbfdcbf40525630db1ce8283eefd43
theenglishway (time) <me@theenglishway.eu>
2021-12-05 13:02:04 +0100

    Cleanup day5 code a bit

--- lib/day_05.ex
+++ lib/day_05.ex
@@ -4,19 +4,18 @@ defmodule AdventOfCode.Day5.Grid do
   defstruct [:max_x, :max_y, points_coverage: %{}]
 
   def new(list_of_vent_lines) when is_list(list_of_vent_lines) do
-    {_, {_, max_x, _}, _} =
+    {_, {max_x, _}, _} =
       list_of_vent_lines
-      |> Enum.max_by(fn {:vent_line, {:coord, start_x, _}, {:coord, end_x, _}} ->
+      |> Enum.max_by(fn {:vent_line, {start_x, _}, {end_x, _}} ->
         max(start_x, end_x)
       end)
 
-    {_, {_, _, max_y}, _} =
+    {_, {_, max_y}, _} =
       list_of_vent_lines
-      |> Enum.max_by(fn {:vent_line, {:coord, _, start_y}, {:coord, _, end_y}} ->
+      |> Enum.max_by(fn {:vent_line, {_, start_y}, {_, end_y}} ->
         max(start_y, end_y)
       end)
 
-    {:grid, max_x, max_y}
     %Grid{max_x: max_x, max_y: max_y}
   end
 
@@ -30,27 +29,24 @@ defmodule AdventOfCode.Day5.Grid do
     end)
   end
 
-  defp add_point_coverage(current, point) do
-    current |> Map.update(point, 1, &(&1 + 1))
-  end
+  defp add_point_coverage(current, point), do: current |> Map.update(point, 1, &(&1 + 1))
 
-  def number_of_points_with_coverage_over(%{points_coverage: coverage}, n) do
-    coverage |> Map.values() |> Enum.count(&(&1 >= n))
-  end
+  def number_of_points_with_coverage_over(%{points_coverage: coverage}, n),
+    do: coverage |> Map.values() |> Enum.count(&(&1 >= n))
 end
 
 defmodule AdventOfCode.Day5.VentLine do
-  def is_vertical?({:vent_line, {_, x1, _}, {_, x2, _}}), do: x1 == x2
-  def is_horizontal?({:vent_line, {_, _, y1}, {_, _, y2}}), do: y1 == y2
-  def is_diagonal?({:vent_line, {_, x1, y1}, {_, x2, y2}}), do: abs(x2 - x1) == abs(y2 - y1)
+  def is_vertical?({:vent_line, {x1, _}, {x2, _}}), do: x1 == x2
+  def is_horizontal?({:vent_line, {_, y1}, {_, y2}}), do: y1 == y2
+  def is_diagonal?({:vent_line, {x1, y1}, {x2, y2}}), do: abs(x2 - x1) == abs(y2 - y1)
   def is_straight?(line), do: is_vertical?(line) or is_horizontal?(line)
 
-  def points_covered(line = {:vent_line, {_, x1, y1}, {_, x2, y2}}) do
+  def points_covered(line = {:vent_line, {x1, y1}, {x2, y2}}) do
     cond do
       is_horizontal?(line) -> for x <- x1..x2, do: {x, y1}
       is_vertical?(line) -> for y <- y1..y2, do: {x1, y}
       is_diagonal?(line) -> for x <- x1..x2, y <- y1..y2, abs(x - x1) == abs(y - y1), do: {x, y}
-      true -> raise "Can not get coverage for non-straight line"
+      true -> raise "Can not get coverage for non-straight or non-diagonal line"
     end
   end
 end
@@ -107,7 +103,7 @@ defmodule AdventOfCode.Day5 do
            list_of_coords <-
              [start_ |> String.split(","), end_ |> String.split(",")] |> Enum.concat(),
            [start_x, start_y, end_x, end_y] <- list_of_coords |> Enum.map(&String.to_integer/1),
-           do: {:vent_line, {:coord, start_x, start_y}, {:coord, end_x, end_y}}
+           do: {:vent_line, {start_x, start_y}, {end_x, end_y}}
     end)
   end
 
