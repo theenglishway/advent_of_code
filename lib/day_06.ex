37f39836f1bd308732c0bb6cdda5052dfb586cd7
theenglishway (time) <me@theenglishway.eu>
2021-12-06 10:56:29 +0100

    A working but painfully slow solution

--- lib/day_06.ex
+++ lib/day_06.ex
@@ -1,10 +1,33 @@
+defmodule AdventOfCode.Day6.Fish do
+  def to_next_day(list) when is_list(list) do
+    {updated, tail} =
+      list
+      |> Enum.reduce({[], []}, fn
+        0, {current, new_tail} -> {current ++ [6], new_tail ++ [8]}
+        element, {current, new_tail} -> {current ++ [element - 1], new_tail}
+      end)
+
+    updated ++ tail
+  end
+end
+
 defmodule AdventOfCode.Day6 do
   @behaviour AdventOfCode.Exercise
   @input "lib/day_06.txt"
+  alias AdventOfCode.Day6.Fish
 
   defmodule FirstHalf do
-    def get_number_of_fishes(fishes_day0, nb_days) do
+    @doc """
 
+      iex> AdventOfCode.Day6.FirstHalf.get_number_of_fishes([3,4,3,1,2], 18)
+      26
+    """
+    def get_number_of_fishes(fishes_day0, nb_days) do
+      1..nb_days
+      |> Enum.reduce(fishes_day0, fn elem, acc ->
+        Fish.to_next_day(acc) |> IO.inspect(label: elem)
+      end)
+      |> Enum.count()
     end
   end
 
@@ -19,9 +42,11 @@ defmodule AdventOfCode.Day6 do
   @impl true
   def get_input() do
     with {:ok, data} <- File.read(@input),
-         do: data
+         do: data |> into_fishes_list
   end
 
+  defp into_fishes_list(string),
+    do: string |> String.split(~r"\W", trim: true) |> Enum.map(&String.to_integer(&1))
 
   @impl true
   def do_output(out), do: inspect(out)
---
+++ test/day06_test.exs
@@ -0,0 +1,4 @@
+defmodule AdventOfCodeDay6Test do
+  use ExUnit.Case
+  doctest AdventOfCode.Day6.FirstHalf
+end
