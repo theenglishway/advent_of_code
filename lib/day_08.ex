18cfd39b9a59aaa5638713d0d55064114b56fba6
theenglishway (time) <me@theenglishway.eu>
2021-12-08 10:50:59 +0100

    Solve first half of day8

--- lib/day_08.ex
+++ lib/day_08.ex
@@ -1,26 +1,41 @@
 defmodule AdventOfCode.Day8 do
   @behaviour AdventOfCode.Exercise
-  @input "lib/day_07.txt"
-  @doc """
+  @input "lib/day_08.txt"
 
-    iex> AdventOfCode.Day8.do_stuff([16,1,2,0,4,2,7,1,2,14])
-    nil
-  """
-  def do_stuff(input) do
+  defp count_digits_with_unique_repr({_, output}) do
+    output
+    |> Enum.count(& (&1 |> MapSet.size) in [2, 3, 4, 7])
+  end
+
+  def get_simple_digits_count(input) do
     input
+    |> Enum.map(& count_digits_with_unique_repr/1)
+    |> Enum.sum()
   end
 
   @impl true
-  def run(input, 1), do: input |> do_stuff()
+  def run(input, 1), do: input |> get_simple_digits_count()
 
   @impl true
   def get_input() do
     with {:ok, data} <- File.read(@input),
-         do: data |> into_crabs_list
+         do: data |> format_input
   end
 
-  defp into_crabs_list(string),
-    do: string |> String.split(~r"\W", trim: true) |> Enum.map(&String.to_integer(&1))
+  def format_input(string),
+    do: string |> String.split("\n", trim: true) |> Enum.map(&format_line/1)
+
+  defp format_line(string) do
+    [signal, output] = string |> String.split("|")
+    {signal |> format_block, output |> format_block}
+  end
+
+  defp format_block(string),
+    do:
+      string
+      |> String.trim()
+      |> String.split(" ", trim: true)
+      |> Enum.map(&(&1 |> String.codepoints() |> MapSet.new()))
 
   @impl true
   def do_output(out), do: inspect(out)
--- test/day08_test.exs
+++ test/day08_test.exs
@@ -1,4 +1,21 @@
 defmodule AdventOfCodeDay8Test do
   use ExUnit.Case
-  doctest AdventOfCode.Day8
+  alias AdventOfCode.Day8
+
+  @input """
+  be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
+  """
+
+  test "should work on first half" do
+    assert @input |> Day8.format_input() |> Day8.get_simple_digits_count() == 26
+  end
 end
