80c69b9f075ffc2cc18f1a24fab34388977429ce
theenglishway (time) <me@theenglishway.eu>
2021-12-09 20:49:51 +0100

    Solve day 9 / first half

--- lib/day_09.ex
+++ lib/day_09.ex
@@ -2,12 +2,24 @@ defmodule AdventOfCode.Day9 do
   @behaviour AdventOfCode.Exercise
   @input "lib/day_09.txt"
 
-  def do_stuff(input) do
+  def get_low_points(input) do
     input
+    |> Stream.filter(fn {{x0, y0}, _} -> is_lower_than_neighbours?(input, x0, y0) end)
+    |> Enum.map(fn {_, height} -> height + 1 end)
+  end
+
+  defp is_lower_than_neighbours?(full_map, x0, y0) do
+    height0 = full_map |> Map.get({x0, y0})
+
+    full_map
+    |> Stream.filter(fn {{x, y}, _} ->
+      (x == x0 and abs(y - y0) == 1) or (abs(x - x0) == 1 and y == y0)
+    end)
+    |> Enum.all?(fn {_, height} -> height > height0 end)
   end
 
   @impl true
-  def run(input, 1), do: input |> do_stuff()
+  def run(input, 1), do: input |> get_low_points()
 
   @impl true
   def get_input() do
@@ -15,8 +27,19 @@ defmodule AdventOfCode.Day9 do
          do: data |> format_input
   end
 
-  def format_input(string), do: string
+  def format_input(string),
+    do:
+      string
+      |> String.split("\n", trim: true)
+      |> Stream.with_index()
+      |> Enum.flat_map(fn {line, y} ->
+        line
+        |> String.codepoints()
+        |> Stream.with_index()
+        |> Stream.map(fn {val, x} -> %{x: x, y: y, height: val |> String.to_integer()} end)
+      end)
+      |> Map.new(fn %{x: x, y: y, height: val} -> {{x, y}, val} end)
 
   @impl true
-  def do_output(out), do: inspect(out)
+  def do_output(list), do: list |> Enum.sum()
 end
--- test/day09_test.exs
+++ test/day09_test.exs
@@ -1,4 +1,16 @@
 defmodule AdventOfCodeDay9Test do
   use ExUnit.Case
-  doctest AdventOfCode.Day9
+  alias AdventOfCode.Day9
+
+  @input """
+  2199943210
+  3987894921
+  9856789892
+  8767896789
+  9899965678
+  """
+
+  test "should work on first half" do
+    assert @input |> Day9.format_input() |> Day9.get_low_points() |> Enum.sort() == [1, 2, 6, 6]
+  end
 end
